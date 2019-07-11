095d1fd59d91166e53d2762260d81d6cee96d80b
jaydenThomson1999 <jaydenthomson1999@gmail.com>
2019-07-11 10:13:18 +1000

    added comments to prepare for partner activity for week 2

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function add(a, b) {
 }
 
 console.log(add(5,10));
+
+//next step
+//find partner
+//  partner will clone my repo
+//  partner will change 'hello git' to 'i caused a merge conflict'
+//  partner will commit with message 'partner changes'
+//
+//attempt merging the adding function branch to the master
+//solve merge conflict
